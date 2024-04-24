8d26f5dde72479143c2a0107b5d2a70d5f11056d
BlacIP <omotoyinbobade15@gmail.com>
2024-04-24 11:49:50 +0100

    cart population

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,35 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Retrieve the current cart from local storage or initialize a new one
+  let cart = getLocalStorage("so-cart");
+  if (!cart) {
+    cart = {};
+  }
+
+  // Check if the product already exists in the cart
+  if (cart[product.Id]) { // Note the change here from product.id to product.Id
+    // If product exists, increase the quantity
+    cart[product.Id].quantity += 1;
+  } else {
+    // If product does not exist, add it with quantity 1
+    cart[product.Id] = {...product, quantity: 1};
+  }
+
+  // Save the updated cart back to local storage
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+// Add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
