28e4c7fb3e1bb0a0f48bfaffbeac0d781199944d
BlacIP <omotoyinbobade15@gmail.com>
2024-05-02 23:10:42 +0100

    bo-individual2

--- src/js/product.js
+++ src/js/product.js
@@ -39,12 +39,12 @@
 // // eslint-disable-next-line no-console
 // console.log(dataSource.findProductById(productId));
 
-import { getParam } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
+const productId = getParams("product");
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
